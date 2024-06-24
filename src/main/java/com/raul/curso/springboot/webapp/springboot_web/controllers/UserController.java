ce5f067750e4cc357ee49513c0bcf606800150ac
Raul <raulsanchezgranda@hotmail.com>
2024-06-24 12:03:18 +0200

    hasta video 14

---
+++ src/main/java/com/raul/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.raul.curso.springboot.webapp.springboot_web.controllers;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class UserController {
+ 
+    @GetMapping("/details")  
+    public String details(Map<String, Object> model){
+        model.put("title", "Hola mundo Spring Boot");
+        model.put("name", "Raúl");
+        model.put("lastname", "Sánchez");
+        return "details";
+    }
+}

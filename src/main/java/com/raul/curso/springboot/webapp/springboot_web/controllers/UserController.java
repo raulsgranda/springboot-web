2eb7f08c1042ac650e192069048926457be9ef81
Raul <raulsanchezgranda@hotmail.com>
2024-06-26 14:56:12 +0200

    Hasta video 17

--- src/main/java/com/raul/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/raul/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -5,15 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.raul.curso.springboot.webapp.springboot_web.models.User;
+
 
 @Controller
 public class UserController {
  
     @GetMapping("/details")  
-    public String details(Map<String, Object> model){
-        model.put("title", "Hola mundo Spring Boot");
-        model.put("name", "Raúl");
-        model.put("lastname", "Sánchez");
+    public String details(Model model){
+        User user = new User("Raúl", "Sánchez");
+        model.addAttribute("title", "Hola mundo Spring Boot");
+        model.addAttribute("user", user);
         return "details";
     }
 }
--- src/main/java/com/raul/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/raul/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -4,19 +4,28 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.raul.curso.springboot.webapp.springboot_web.models.User;
 
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
  
-    @GetMapping("/details2")  
+    @GetMapping("/details")  
+    
     public  Map<String, Object> details(){
+        User user = new User("Raúl", "Sánchez");
         Map<String, Object> body = new HashMap<>();
+        
         body.put("title", "Hola mundo Spring Boot");
-        body.put("name", "Raúl");
-        body.put("lastname", "Sánchez");
+        body.put("user", user);
+     
+       
         return body;
     }
 }
